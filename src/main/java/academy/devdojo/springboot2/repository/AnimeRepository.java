a66083ff78140727f85445e7ee38f2b92d776b79
Arthur Cech Francisco <arthurcech@gmail.com>
2022-04-23 15:15:01 -0300

    feat: add DTO pattern and communication between controller-service-repository

--- src/main/java/academy/devdojo/springboot2/repository/AnimeRepository.java
+++ src/main/java/academy/devdojo/springboot2/repository/AnimeRepository.java
@@ -1,4 +1,7 @@
 package academy.devdojo.springboot2.repository;
 
-public interface AnimeRepository {
+import academy.devdojo.springboot2.domain.Anime;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnimeRepository extends JpaRepository<Anime, Long> {
 }
