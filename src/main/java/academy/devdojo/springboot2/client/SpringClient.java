131c6dad0d4289d9badf015f09f662ec996ea07f
Arthur Cech Francisco <arthurcech@gmail.com>
2022-04-29 18:48:12 -0300

    feat: add api requests through RestTemplate

---
+++ src/main/java/academy/devdojo/springboot2/client/SpringClient.java
@@ -0,0 +1,72 @@
+package academy.devdojo.springboot2.client;
+
+import academy.devdojo.springboot2.domain.Anime;
+import academy.devdojo.springboot2.requests.AnimePostRequestBody;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+@Log4j2
+public class SpringClient {
+
+    public static void main(String[] args) {
+//        ResponseEntity<Anime> entity = new RestTemplate().getForEntity("http://localhost:8080/animes/2", Anime.class);
+
+//        Anime entity = new RestTemplate().getForObject("http://localhost:8080/animes/{id}", Anime.class, 4);
+//        log.info(entity);
+
+//        Anime[] animes = new RestTemplate().getForObject("http://localhost:8080/animes", Anime[].class);
+//        log.info(Arrays.toString(animes));
+
+//        ResponseEntity<List<Anime>> exchange = new RestTemplate().exchange(
+//                "http://localhost:8080/animes",
+//                HttpMethod.GET,
+//                null,
+//                new ParameterizedTypeReference<List<Anime>>() {
+//                });
+//        log.info(exchange.getBody());
+
+//        AnimePostRequestBody kingdom = AnimePostRequestBody.builder().name("Kingdom").build();
+//        Anime kingdomSaved = new RestTemplate().postForObject("http://localhost:8080/animes", kingdom, Anime.class);
+//        log.info("saved anime {}", kingdomSaved);
+
+        AnimePostRequestBody samuraiChamploo = AnimePostRequestBody.builder().name("Samurai Champloo").build();
+        ResponseEntity<Anime> samuraiChamplooSaved = new RestTemplate().exchange(
+                "http://localhost:8080/animes",
+                HttpMethod.POST,
+                new HttpEntity<>(samuraiChamploo, createJsonHeader()),
+                Anime.class);
+        log.info("saved anime {}", samuraiChamplooSaved);
+
+        Anime animeToBeUpdated = samuraiChamplooSaved.getBody();
+        animeToBeUpdated.setName("Samurai Champloo New");
+
+        ResponseEntity<Void> samuraiChamplooUpdated = new RestTemplate().exchange(
+                "http://localhost:8080/animes",
+                HttpMethod.PUT,
+                new HttpEntity<>(animeToBeUpdated, createJsonHeader()),
+                Void.class);
+
+        log.info(samuraiChamplooUpdated);
+
+        ResponseEntity<Void> samuraiChamplooDeleted = new RestTemplate().exchange(
+                "http://localhost:8080/animes/{id}",
+                HttpMethod.DELETE,
+                null,
+                Void.class,
+                animeToBeUpdated.getId());
+
+        log.info(samuraiChamplooDeleted);
+    }
+
+    private static HttpHeaders createJsonHeader() {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        return httpHeaders;
+    }
+
+}
--- src/main/java/academy/devdojo/springboot2/requests/AnimePutRequestBody.java
+++ src/main/java/academy/devdojo/springboot2/requests/AnimePutRequestBody.java
@@ -3,11 +3,12 @@ package academy.devdojo.springboot2.requests;
 import lombok.Data;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Data
 public class AnimePutRequestBody {
 
-    @NotEmpty(message = "The anime id cannot be empty or null")
+    @NotNull(message = "The anime id cannot be null")
     private Long id;
 
     @NotEmpty(message = "The anime name cannot be empty or null")
